41da40c01b7c180299d3f07d3d8a4dde126c5942
Antony Akimchenko <metalization25@gmail.com>
2019-09-21 22:34:53 +0300

    Testing MainFragmentAdapter with test text data

--- app/src/main/java/com/example/uberremaind/MainFragment.java
+++ app/src/main/java/com/example/uberremaind/MainFragment.java
@@ -4,14 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFragment extends Fragment {
 
@@ -29,19 +31,44 @@ public class MainFragment extends Fragment {
 
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false));
-        recyclerView.setAdapter(new MainFragmentAdapter());
+        MainFragmentAdapter adapter = new MainFragmentAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.setItems(getTestItems());
+    }
+
+    private ArrayList<Item> getTestItems() {
+        ArrayList<Item> list = new ArrayList<>();
+        for (int item = 0; item < 50; item++) {
+            Item addedItem = new Item();
+            addedItem.setTitle("slkdvnlakfnvlaildsnfkbuskd");
+            addedItem.setDescription("sjdhvlaisdhgvlaks,dbv,kashdvk,asudb  kaus,hdvajsdn uka,ds");
+            list.add(addedItem);
+        }
+        return list;
     }
 
     private class MainFragmentAdapter extends RecyclerView.Adapter<MainFragmentAdapter.ViewHolder> {
 
-        private class ViewHolder extends RecyclerView.ViewHolder {
+        private ArrayList<Item> items = new ArrayList<>();
 
-            private ImageView negro;
+        private class ViewHolder extends RecyclerView.ViewHolder {
+            private TextView title, description;
 
             private ViewHolder(@NonNull View itemView) {
                 super(itemView);
-                negro = itemView.findViewById(R.id.picture);
+                title = itemView.findViewById(R.id.title);
+                description = itemView.findViewById(R.id.description);
             }
+        }
+
+        void setItems(@Nullable List<Item> items) {
+            this.items.clear();
+
+            if (items != null)
+                this.items.addAll(items);
+
+            notifyDataSetChanged();
         }
 
         @NonNull
@@ -55,14 +82,39 @@ public class MainFragment extends Fragment {
             MainActivity activity = (MainActivity) getActivity();
             if (activity == null) return;
 
-            if (position % 10 == 0) {
+            holder.title.setText(items.get(position).getTitle());
+            holder.description.setText(items.get(position).getDescription());
+
+            //All *if* expressions must be excessive
+           /*if (position % 2 == 0)
                 holder.itemView.setBackgroundColor(ContextCompat.getColor(activity, R.color.colorPrimaryDark));
-            }
+            else
+                holder.itemView.setBackgroundColor(Color.TRANSPARENT);*/
         }
 
         @Override
         public int getItemCount() {
-            return 15;
+            return items.size();
+        }
+    }
+
+    class Item {
+        private String title, description;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
         }
     }
 }
